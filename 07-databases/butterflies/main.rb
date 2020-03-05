0d319c777abf0eadd28b530542d20d031afb9bfd
JT <certainthings@gmail.com>
2020-03-05 14:44:20 +1100

    complete CRUD for butterflies

--- 07-databases/butterflies/main.rb
+++ 07-databases/butterflies/main.rb
@@ -8,9 +8,7 @@ end
 
 # INDEX
 get '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterflies = db.execute 'SELECT * FROM butterflies'
+  @butterflies = query_db 'SELECT * FROM butterflies'
   erb :butterflies_index
 end
 
@@ -21,16 +19,41 @@ end
 
 # CREATE
 post '/butterflies' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.execute "INSERT INTO butterflies (name, family, image) VALUES '#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }'"
+  query_db "INSERT INTO butterflies (name, family, image) VALUES ('#{ params[:name] }', '#{ params[:family] }', '#{ params[:image] }')"
   redirect to('/butterflies')
 end
 
 # SHOW
 get '/butterflies/:id' do
-  db = SQLite3::Database.new 'database.sqlite3'
-  db.results_as_hash = true
-  @butterfly = db.execute "SELECT * FROM butterflies WHERE id=#{ params[:id] }" # Array
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }" # Array
   @butterfly = @butterfly.first
   erb :butterflies_show
+end
+
+# EDIT
+get '/butterflies/:id/edit' do
+  @butterfly = query_db "SELECT * FROM butterflies WHERE id=#{ params[:id] }" # Array
+  @butterfly = @butterfly.first
+  erb :butterflies_edit
+end
+
+# UPDATE
+post '/butterflies/:id' do
+  query_db "UPDATE butterflies SET name='#{ params[:name] }', family='#{ params[:family] }', image='#{ params[:image] }' WHERE id=#{ params[:id] }"
+  redirect to("/butterflies/#{ params[:id] }") # GET request
+end
+
+# DESTROY
+get '/butterflies/:id/delete' do
+  query_db "DELETE FROM butterflies WHERE id=#{ params[:id] }"
+  redirect to('/butterflies')
+end
+
+def query_db(sql_statement)
+  puts sql_statement # Optional but nice for debugging.
+  db = SQLite3::Database.new 'database.sqlite3'
+  db.results_as_hash = true
+  results = db.execute sql_statement
+  db.close
+  results # Implicitly return
 end
