855162a7529a50fac313f707bad58269b910beaa
JT <certainthings@gmail.com>
2020-03-09 13:00:20 +1100

    a first look at rails routing, controllers and views

---
+++ 08-rails/rails-intro/app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def hello
+    render :hello
+  end
+
+  def goodbye
+    render :goodbye
+  end
+
+  def welcome
+    render :welcome
+  end
+
+  def about
+    render :about
+  end
+end
