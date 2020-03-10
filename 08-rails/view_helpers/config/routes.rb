a7d47a1a04dbf62329b090fd09fbb1997ddc915f
JT <certainthings@gmail.com>
2020-03-10 11:51:55 +1100

    view helpers part 2

--- 08-rails/view_helpers/config/routes.rb
+++ 08-rails/view_helpers/config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
-  get '/numbers' => 'pages#numbers'
+  get '/microsoft-numb3rs' => 'pages#numbers', :as => :numbers # numbers_path
   get '/text' => 'pages#text'
   get '/assets' => 'pages#assets'
   get '/url' => 'pages#url'
