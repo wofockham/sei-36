b2fc4616d85e31bbd56537f2e9cf596c526bcc09
JT <certainthings@gmail.com>
2020-03-23 16:51:13 +1100

    note that there is no httparty for js

--- 10-ajax/ajax-intro/numbers.rb
+++ 10-ajax/ajax-intro/numbers.rb
@@ -1,3 +1,6 @@
+# This is just an example of how easy it is to retrieve content from a URL in Ruby.
+# Javascript will take a lot more effort.
+
 require 'httparty'
 
 data = HTTParty.get 'http://numbersapi.com/random/trivia'
