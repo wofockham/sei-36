db63e030d019e66406f3e89775ff5a4bd43d60b2
JT <certainthings@gmail.com>
2020-03-25 14:27:49 +1100

    ajax request works

--- 10-ajax/flickr-search/js/search.js
+++ 10-ajax/flickr-search/js/search.js
@@ -1,9 +1,22 @@
+const searchFlickr = function (words) {
+  console.log('Searching Flickr for', words);
+
+  const flickrURL = 'https://api.flickr.com/services/rest?jsoncallback=?';
+  $.getJSON(flickrURL, {
+    method: 'flickr.photos.search',
+    api_key: '2f5ac274ecfac5a455f38745704ad084', // This is not a secret key
+    text: words,
+    format: 'json'
+  }).done(function (results) {
+    console.log( results );
+  });
+};
+
 $(document).ready(function () {
   $('form#search').on('submit', function (event) {
     event.preventDefault(); // Don't go anywhere.
 
     const term = $('#query').val();
-    // search Flickr for that term
-    // .done(): display the results
+    searchFlickr( term );
   });
 });
