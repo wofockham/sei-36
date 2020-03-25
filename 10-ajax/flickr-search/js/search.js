1ca261ffee3381c2575b5d41a65b8b855b8289e9
JT <certainthings@gmail.com>
2020-03-25 14:40:40 +1100

    generate URL

--- 10-ajax/flickr-search/js/search.js
+++ 10-ajax/flickr-search/js/search.js
@@ -1,3 +1,22 @@
+const generateURL = function (p) {
+  return [
+    'http://farm',
+    p.farm,
+    '.static.flickr.com/',
+    p.server,
+    '/',
+    p.id,
+    '_',
+    p.secret,
+    '_q.jpg' // Change this to something else for different sizes (see docs)
+  ].join('');
+}
+
+const showImages = function (results) {
+  console.log( results.photos.photo );
+  console.log( generateURL( results.photos.photo[0] )  );
+};
+
 const searchFlickr = function (words) {
   console.log('Searching Flickr for', words);
 
@@ -7,9 +26,7 @@ const searchFlickr = function (words) {
     api_key: '2f5ac274ecfac5a455f38745704ad084', // This is not a secret key
     text: words,
     format: 'json'
-  }).done(function (results) {
-    console.log( results );
-  });
+  }).done(showImages);
 };
 
 $(document).ready(function () {
