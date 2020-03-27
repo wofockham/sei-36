3f8d61b5f70bcfb4c56e3c94732b0ebf824ca5e2
JT <certainthings@gmail.com>
2020-03-27 13:01:08 +1100

    <HelloReact>

---
+++ 12-react/hello-react/src/HelloWorld.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'; // destructuring
+
+class HelloWorld extends Component {
+  render() {
+    return (
+      <h2>Hello World</h2>
+    );
+  }
+}
+
+export default HelloWorld;
