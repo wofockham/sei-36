d05dfa1c458fa953ac6e60857090bdc1d216b196
JT <certainthings@gmail.com>
2020-03-27 15:08:11 +1100

    social network

---
+++ 12-react/social-network/src/components/Profile.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      height: 100
+    };
+    this._zoomIn = this._zoomIn.bind(this);
+    this._zoomOut = this._zoomOut.bind(this);
+  }
+
+  _zoomIn() {
+    this.setState({height: this.state.height + 10});
+  }
+
+  _zoomOut() {
+    this.setState({height: this.state.height - 10});
+  }
+
+  render() {
+    const { name, age, motto, pic } = this.props; // More destructuring.
+
+    return (
+      <div>
+        <h2>{ name }</h2>
+        <h3>Age: { age }</h3>
+        <p>{ motto }</p>
+        <div>
+          <button onClick={ this._zoomIn }>+</button>
+          <button onClick={ this._zoomOut }>-</button>
+        </div>
+        <img src={ pic } alt={ `This is ${ name } ` } height={ this.state.height } />
+      </div>
+    );
+  }
+};
+
+export default Profile;
