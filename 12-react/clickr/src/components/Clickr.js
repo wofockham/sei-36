0d2ce34efb98a70ba9cba544045fa61203ede3a3
JT <certainthings@gmail.com>
2020-03-27 15:08:00 +1100

    click counter

--- 12-react/clickr/src/components/Clickr.js
+++ 12-react/clickr/src/components/Clickr.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
 
 class Clickr extends Component {
-  click() {
-    console.log('you clicked');
+  constructor() {
+    super();
+    this.state = {
+      clicks: 0
+    };
+
+    // Mystical mumbojumbo that will be explained later:
+    // Bind the event handler to the object permanently.
+    this._incrementClicks = this._incrementClicks.bind( this );
+  }
+
+  // _methodName indicates that methodName is an event handler.
+  _incrementClicks() {
+    // this.state.clicks++; // Mutation: doesn't work.
+    this.setState({clicks: this.state.clicks + 1}); // No mutation.
   }
 
   render() {
     return (
-      <button onClick={ this.click }>0 clicks so far</button>
+      <button onClick={ this._incrementClicks }>{ this.state.clicks } clicks so far</button>
     );
   }
 }
