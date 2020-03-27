665db69066c49c8368e3c0eea23d88d3c2d35570
JT <certainthings@gmail.com>
2020-03-27 16:23:30 +1100

    social network, <Calculator />

--- 12-react/social-network/src/components/App.js
+++ 12-react/social-network/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Profile from './Profile';
+import Clock from './Clock';
 
 function App() {
   return (
     <div className="App">
+
+      <Clock />
       <Profile name="Groucho" age="101" motto="No gods no masters" pic="http://www.fillmurray.com/400/400" />
       <Profile name="Harpo" age="98" motto="" pic="http://www.fillmurray.com/401/400" />
       <Profile name="Chico" age="99" motto="No gods no masters" pic="http://www.fillmurray.com/400/401" />
+
     </div>
   );
 }
---
+++ 12-react/social-network/src/components/Clock.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+// TODO:
+// Create a <Time /> component that shows how many seconds ago the user loaded the page.
+// You loaded this page 19 seconds ago.
+class Clock extends Component {
+  constructor() {
+    super();
+    this.state = { time: (new Date()).toString() };
+
+    setInterval(() => {
+      this.setState({ time: (new Date()).toString() });
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <h1>{ this.state.time }</h1>
+    );
+  }
+}
+
+export default Clock;
