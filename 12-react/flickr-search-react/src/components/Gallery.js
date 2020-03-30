4ac3a87727aad94e3895f725e7deff96787146e6
JT <certainthings@gmail.com>
2020-03-30 15:06:02 +1100

    functional components

--- 12-react/flickr-search-react/src/components/Gallery.js
+++ 12-react/flickr-search-react/src/components/Gallery.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Gallery extends Component {
-  render() {
-    return (
-      <div>
-        { this.props.images.map( (url) => <img src={ url } /> ) }
-      </div>
-    );
-  }
-}
+// class Gallery extends Component {
+//   render() {
+//     return (
+//       <div>
+//         { this.props.images.map( (url) => <Thumbnail src={ url } /> ) }
+//       </div>
+//     );
+//   }
+// }
+
+// Functional components receive a single object parameter: props.
+const Gallery = (props) => {
+  console.log( '<Gallery />', props );
+  return (
+    <div>
+      { props.images.map( (url) => <Thumbnail picture={ url } /> ) }
+    </div>
+  );
+};
+
+// Incredibly: this is a function with implicit return.
+const Thumbnail = (props) => <img src={ props.picture } alt="" />;
+
 
 export default Gallery;
