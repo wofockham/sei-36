4f94a28a40f121aab10e82a76c23ca06bb18b0e5
JT <certainthings@gmail.com>
2020-04-14 13:00:37 +1000

    faster ruby recursion

--- 13-comp.sci/recursion/fibonacci.rb
+++ 13-comp.sci/recursion/fibonacci.rb
@@ -19,9 +19,29 @@ def fibonacci_recursive(n)
   end
 end
 
-# TODO: Rewrite the recursive version to be faster:
-# 1. Memoisation
-# 2. Iterative recursion
+# Memoisation
+def fib_memo(n)
+  @fib ||= {} # @fib = @fib || {}
+
+  if @fib[n]
+    @fib[n]
+  elsif n == 1 || n == 2
+    1
+  else
+    result = fib_memo(n-1) + fib_memo(n-2)
+    @fib[n] = result
+    result
+  end
+end
+
+# Iterative recursion
+def fib_iter(n, a=1, b=1)
+  if n == 1 || n == 2
+    b
+  else
+    fib_iter(n-1, b, a+b)
+  end
+end
 
 require 'pry'
 binding.pry
