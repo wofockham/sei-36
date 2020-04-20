11c39ec10676bbdfb2c97db677f39e7029f47b46
JT <certainthings@gmail.com>
2020-04-20 11:12:03 +1000

    vocab server: schema

---
+++ 14-advanced/vocab/vocab-server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.listen(port);
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
